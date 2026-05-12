fa47236e40812e04ec3fc1666df7312e41171226
Flameflr <yannickgetto47@gmail.com>
2026-05-12 19:54:52 +0200

    initial linux nvim conig

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("core.lazy")
+require("config.remap")
+require("plugins.colorscheme")
+require("plugins.fuzzyfinder")
+require("plugins.tree")
+require("plugins.oil")
+
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
