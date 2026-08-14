88a73f523e7fd4ee08e76804e6b85a6cd4e9d3b0
Yannick <yannickgetto47@gmail.com>
2026-08-14 10:41:03 +0200

    transparent mode for hyprland

--- init.lua
+++ init.lua
@@ -10,3 +10,4 @@ vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.api.nvim_set_hl(0, 'Normal', {bg = 'NONE'})
--- lua/plugins/fuzzyfinder.lua
+++ lua/plugins/fuzzyfinder.lua
@@ -14,16 +14,14 @@ return
 
     local builtin = require('telescope.builtin')
 
-    vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-    vim.keymap.set("n", "<leader>fr", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    vim.keymap.set("n", "<leader>fh", ":Telescope find_files hidden=true <CR>")
-
 
     vim.keymap.set("n", "<leader>en", function()
      builtin.find_files { cwd = vim.fn.stdpath "config" }
     end)
+
+    vim.keymap.set("n", "<leader>hy", function() builtin.find_files { cwd = "~/.config/hypr" }
+    end)
+
   end,
 
 }
